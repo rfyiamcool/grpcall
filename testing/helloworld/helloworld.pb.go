f3e27e8420b4d130d89966946eb2fbf95968a232
rfyiamcool <rfyiamcool@163.com>
2019-03-18 19:22:48 +0800

    update testing pb

--- testing/helloworld/helloworld.pb.go
+++ testing/helloworld/helloworld.pb.go
@@ -102,6 +102,110 @@ func (m *HelloReply) GetMessage() string {
 	return ""
 }
 
+// The request message containing the user's name.
+type NotifyReq struct {
+	Nick                 string   `protobuf:"bytes,1,opt,name=nick,proto3" json:"nick,omitempty"`
+	Addr                 string   `protobuf:"bytes,2,opt,name=addr,proto3" json:"addr,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *NotifyReq) Reset()         { *m = NotifyReq{} }
+func (m *NotifyReq) String() string { return proto.CompactTextString(m) }
+func (*NotifyReq) ProtoMessage()    {}
+func (*NotifyReq) Descriptor() ([]byte, []int) {
+	return fileDescriptor_17b8c58d586b62f2, []int{2}
+}
+
+func (m *NotifyReq) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_NotifyReq.Unmarshal(m, b)
+}
+func (m *NotifyReq) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_NotifyReq.Marshal(b, m, deterministic)
+}
+func (m *NotifyReq) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_NotifyReq.Merge(m, src)
+}
+func (m *NotifyReq) XXX_Size() int {
+	return xxx_messageInfo_NotifyReq.Size(m)
+}
+func (m *NotifyReq) XXX_DiscardUnknown() {
+	xxx_messageInfo_NotifyReq.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_NotifyReq proto.InternalMessageInfo
+
+func (m *NotifyReq) GetNick() string {
+	if m != nil {
+		return m.Nick
+	}
+	return ""
+}
+
+func (m *NotifyReq) GetAddr() string {
+	if m != nil {
+		return m.Addr
+	}
+	return ""
+}
+
+// The response message containing the greetings
+type NotifyReply struct {
+	Message              string   `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
+	Phone                string   `protobuf:"bytes,2,opt,name=phone,proto3" json:"phone,omitempty"`
+	Car                  string   `protobuf:"bytes,3,opt,name=car,proto3" json:"car,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *NotifyReply) Reset()         { *m = NotifyReply{} }
+func (m *NotifyReply) String() string { return proto.CompactTextString(m) }
+func (*NotifyReply) ProtoMessage()    {}
+func (*NotifyReply) Descriptor() ([]byte, []int) {
+	return fileDescriptor_17b8c58d586b62f2, []int{3}
+}
+
+func (m *NotifyReply) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_NotifyReply.Unmarshal(m, b)
+}
+func (m *NotifyReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_NotifyReply.Marshal(b, m, deterministic)
+}
+func (m *NotifyReply) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_NotifyReply.Merge(m, src)
+}
+func (m *NotifyReply) XXX_Size() int {
+	return xxx_messageInfo_NotifyReply.Size(m)
+}
+func (m *NotifyReply) XXX_DiscardUnknown() {
+	xxx_messageInfo_NotifyReply.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_NotifyReply proto.InternalMessageInfo
+
+func (m *NotifyReply) GetMessage() string {
+	if m != nil {
+		return m.Message
+	}
+	return ""
+}
+
+func (m *NotifyReply) GetPhone() string {
+	if m != nil {
+		return m.Phone
+	}
+	return ""
+}
+
+func (m *NotifyReply) GetCar() string {
+	if m != nil {
+		return m.Car
+	}
+	return ""
+}
+
 type SimpleData struct {
 	Msg                  string   `protobuf:"bytes,1,opt,name=msg,proto3" json:"msg,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -113,7 +217,7 @@ func (m *SimpleData) Reset()         { *m = SimpleData{} }
 func (m *SimpleData) String() string { return proto.CompactTextString(m) }
 func (*SimpleData) ProtoMessage()    {}
 func (*SimpleData) Descriptor() ([]byte, []int) {
-	return fileDescriptor_17b8c58d586b62f2, []int{2}
+	return fileDescriptor_17b8c58d586b62f2, []int{4}
 }
 
 func (m *SimpleData) XXX_Unmarshal(b []byte) error {
@@ -152,7 +256,7 @@ func (m *ServerStreamData) Reset()         { *m = ServerStreamData{} }
 func (m *ServerStreamData) String() string { return proto.CompactTextString(m) }
 func (*ServerStreamData) ProtoMessage()    {}
 func (*ServerStreamData) Descriptor() ([]byte, []int) {
-	return fileDescriptor_17b8c58d586b62f2, []int{3}
+	return fileDescriptor_17b8c58d586b62f2, []int{5}
 }
 
 func (m *ServerStreamData) XXX_Unmarshal(b []byte) error {
@@ -183,6 +287,8 @@ func (m *ServerStreamData) GetMsg() string {
 func init() {
 	proto.RegisterType((*HelloRequest)(nil), "helloworld.HelloRequest")
 	proto.RegisterType((*HelloReply)(nil), "helloworld.HelloReply")
+	proto.RegisterType((*NotifyReq)(nil), "helloworld.NotifyReq")
+	proto.RegisterType((*NotifyReply)(nil), "helloworld.NotifyReply")
 	proto.RegisterType((*SimpleData)(nil), "helloworld.SimpleData")
 	proto.RegisterType((*ServerStreamData)(nil), "helloworld.ServerStreamData")
 }
@@ -190,23 +296,30 @@ func init() {
 func init() { proto.RegisterFile("helloworld.proto", fileDescriptor_17b8c58d586b62f2) }
 
 var fileDescriptor_17b8c58d586b62f2 = []byte{
-	// 249 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x91, 0x41, 0x4b, 0xc3, 0x40,
-	0x10, 0x85, 0xb3, 0x28, 0xc6, 0x3e, 0x3c, 0xc4, 0x11, 0x24, 0x04, 0x11, 0x59, 0x44, 0x7a, 0x2a,
-	0x25, 0xde, 0x15, 0x54, 0x50, 0xf1, 0x22, 0x89, 0x7f, 0x60, 0xdb, 0x0e, 0x35, 0xb0, 0x6b, 0xd6,
-	0xcd, 0xaa, 0xe4, 0xdf, 0x4b, 0xd2, 0x86, 0xa6, 0x12, 0x7b, 0x9b, 0x79, 0xf3, 0xed, 0xbc, 0x79,
-	0x2c, 0xa2, 0x77, 0xd6, 0xba, 0xfc, 0x29, 0x9d, 0x5e, 0x4c, 0xac, 0x2b, 0x7d, 0x49, 0xd8, 0x28,
-	0x52, 0xe2, 0xe8, 0xa9, 0xe9, 0x32, 0xfe, 0xfc, 0xe2, 0xca, 0x13, 0x61, 0xff, 0x43, 0x19, 0x8e,
-	0xc5, 0x85, 0x18, 0x8f, 0xb2, 0xb6, 0x96, 0x57, 0xc0, 0x9a, 0xb1, 0xba, 0xa6, 0x18, 0xa1, 0xe1,
-	0xaa, 0x52, 0xcb, 0x0e, 0xea, 0x5a, 0x79, 0x0e, 0xe4, 0x85, 0xb1, 0x9a, 0x1f, 0x94, 0x57, 0x14,
-	0x61, 0xcf, 0x54, 0xcb, 0x35, 0xd3, 0x94, 0xf2, 0x12, 0x51, 0xce, 0xee, 0x9b, 0x5d, 0xee, 0x1d,
-	0x2b, 0x33, 0x4c, 0xa5, 0xcf, 0x08, 0x1f, 0x1d, 0xb3, 0x67, 0x47, 0x37, 0x38, 0xcc, 0x55, 0xdd,
-	0x7a, 0x53, 0x3c, 0xe9, 0xe5, 0xe8, 0x9f, 0x9c, 0x9c, 0x0e, 0x4c, 0xac, 0xae, 0x65, 0x90, 0xbe,
-	0xe1, 0xf8, 0xae, 0x58, 0x14, 0x2b, 0xbb, 0xc6, 0xba, 0x98, 0x33, 0xdd, 0x22, 0x6c, 0xc4, 0xec,
-	0xf5, 0x9e, 0xb6, 0x5e, 0x6e, 0x4e, 0x4f, 0xfe, 0xd1, 0x65, 0x30, 0x16, 0x53, 0x91, 0xce, 0x70,
-	0xd2, 0x8f, 0xd1, 0xed, 0x7d, 0xc1, 0x68, 0x25, 0x64, 0x76, 0x4e, 0x67, 0x5b, 0x1b, 0xfe, 0x84,
-	0x4e, 0x76, 0x4e, 0x65, 0x30, 0x15, 0xb3, 0x83, 0xf6, 0xa7, 0xae, 0x7f, 0x03, 0x00, 0x00, 0xff,
-	0xff, 0x4c, 0x90, 0x45, 0x0b, 0xbd, 0x01, 0x00, 0x00,
+	// 365 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0x53, 0x5d, 0x4b, 0xeb, 0x40,
+	0x10, 0x6d, 0x6e, 0x6f, 0x9b, 0xdb, 0xb9, 0xf7, 0xa1, 0x77, 0xfc, 0x0a, 0x41, 0x44, 0x16, 0x91,
+	0x3e, 0x95, 0x92, 0x3e, 0x89, 0x5f, 0xa0, 0x42, 0x0b, 0x82, 0x4a, 0x22, 0xf8, 0xbc, 0x4d, 0xc6,
+	0x76, 0x69, 0xd2, 0xa4, 0x9b, 0xa8, 0xe4, 0x1f, 0xf8, 0xab, 0xfc, 0x6d, 0xb2, 0x49, 0xd3, 0xa6,
+	0x52, 0x0b, 0xf6, 0x6d, 0xe6, 0xcc, 0x39, 0x87, 0xd9, 0x33, 0x2c, 0x34, 0x47, 0xe4, 0xfb, 0xe1,
+	0x5b, 0x28, 0x7d, 0xaf, 0x1d, 0xc9, 0x30, 0x09, 0x11, 0x16, 0x08, 0x63, 0xf0, 0xaf, 0xaf, 0x3a,
+	0x9b, 0xa6, 0x2f, 0x14, 0x27, 0x88, 0xf0, 0x7b, 0xc2, 0x03, 0x32, 0xb4, 0x43, 0xad, 0xd5, 0xb0,
+	0xb3, 0x9a, 0x1d, 0x03, 0xcc, 0x38, 0x91, 0x9f, 0xa2, 0x01, 0x7a, 0x40, 0x71, 0xcc, 0x87, 0x05,
+	0xa9, 0x68, 0x59, 0x17, 0x1a, 0x77, 0x61, 0x22, 0x9e, 0x53, 0x9b, 0xa6, 0x99, 0x91, 0x70, 0xc7,
+	0x73, 0x23, 0xe1, 0x8e, 0x15, 0xc6, 0x3d, 0x4f, 0x1a, 0xbf, 0x72, 0x4c, 0xd5, 0xec, 0x1e, 0xfe,
+	0x16, 0xa2, 0xb5, 0xee, 0xb8, 0x0d, 0xb5, 0x68, 0x14, 0x4e, 0x68, 0xa6, 0xce, 0x1b, 0x6c, 0x42,
+	0xd5, 0xe5, 0xd2, 0xa8, 0x66, 0x98, 0x2a, 0xd9, 0x01, 0x80, 0x23, 0x82, 0xc8, 0xa7, 0x1b, 0x9e,
+	0x70, 0x35, 0x0f, 0xe2, 0xe1, 0xcc, 0x4b, 0x95, 0xec, 0x08, 0x9a, 0x0e, 0xc9, 0x57, 0x92, 0x4e,
+	0x22, 0x89, 0x07, 0xab, 0x59, 0xd6, 0xbb, 0x06, 0x7a, 0x4f, 0x12, 0x25, 0x24, 0xf1, 0x02, 0xfe,
+	0x38, 0x3c, 0xcd, 0x22, 0x40, 0xa3, 0x5d, 0x8a, 0xb3, 0x9c, 0x9c, 0xb9, 0xbb, 0x62, 0x12, 0xf9,
+	0x29, 0xab, 0xe0, 0x39, 0xe8, 0x0e, 0x4f, 0x7b, 0x42, 0xfa, 0x9b, 0xc8, 0xad, 0x0f, 0x0d, 0xea,
+	0x79, 0x44, 0x78, 0x96, 0x6d, 0xf2, 0xa4, 0x38, 0xb8, 0x53, 0x16, 0xcc, 0x73, 0x37, 0xf7, 0x56,
+	0xc1, 0xf9, 0x1e, 0xa7, 0xd9, 0x1e, 0x7d, 0x3e, 0xe2, 0x1b, 0x88, 0x4f, 0xa0, 0xa6, 0xc4, 0xe2,
+	0xe7, 0x52, 0xeb, 0x11, 0xfe, 0x5f, 0x09, 0x4f, 0xe4, 0x79, 0xab, 0xec, 0x85, 0x4b, 0x78, 0x09,
+	0xba, 0x02, 0xed, 0x87, 0x6b, 0x5c, 0x7a, 0xfa, 0xe2, 0x76, 0xe6, 0x37, 0x38, 0xab, 0xb4, 0xb4,
+	0x8e, 0x66, 0x0d, 0x60, 0xab, 0x7c, 0xc7, 0xc2, 0xf7, 0x16, 0x1a, 0x39, 0x60, 0x47, 0x2e, 0xee,
+	0x2f, 0x39, 0x7c, 0xb9, 0xba, 0xb9, 0x76, 0xca, 0x2a, 0x1d, 0x6d, 0x50, 0xcf, 0x3e, 0x4c, 0xf7,
+	0x33, 0x00, 0x00, 0xff, 0xff, 0x3d, 0xb3, 0xda, 0xc6, 0x44, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -223,6 +336,7 @@ const _ = grpc.SupportPackageIsVersion4
 type GreeterClient interface {
 	// Sends a greeting
 	SayHello(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error)
+	SayGirl(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error)
 }
 
 type greeterClient struct {
@@ -242,10 +356,20 @@ func (c *greeterClient) SayHello(ctx context.Context, in *HelloRequest, opts ...
 	return out, nil
 }
 
+func (c *greeterClient) SayGirl(ctx context.Context, in *HelloRequest, opts ...grpc.CallOption) (*HelloReply, error) {
+	out := new(HelloReply)
+	err := c.cc.Invoke(ctx, "/helloworld.Greeter/SayGirl", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // GreeterServer is the server API for Greeter service.
 type GreeterServer interface {
 	// Sends a greeting
 	SayHello(context.Context, *HelloRequest) (*HelloReply, error)
+	SayGirl(context.Context, *HelloRequest) (*HelloReply, error)
 }
 
 func RegisterGreeterServer(s *grpc.Server, srv GreeterServer) {
@@ -270,6 +394,24 @@ func _Greeter_SayHello_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Greeter_SayGirl_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(HelloRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(GreeterServer).SayGirl(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/helloworld.Greeter/SayGirl",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(GreeterServer).SayGirl(ctx, req.(*HelloRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Greeter_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "helloworld.Greeter",
 	HandlerType: (*GreeterServer)(nil),
@@ -277,6 +419,142 @@ var _Greeter_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "SayHello",
 			Handler:    _Greeter_SayHello_Handler,
+		},
+		{
+			MethodName: "SayGirl",
+			Handler:    _Greeter_SayGirl_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "helloworld.proto",
+}
+
+// NotifyClient is the client API for Notify service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type NotifyClient interface {
+	// Sends notify
+	SayWorld(ctx context.Context, in *NotifyReq, opts ...grpc.CallOption) (*NotifyReply, error)
+	SayHaha(ctx context.Context, in *NotifyReq, opts ...grpc.CallOption) (*NotifyReply, error)
+	SayHi(ctx context.Context, in *NotifyReq, opts ...grpc.CallOption) (*NotifyReply, error)
+}
+
+type notifyClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewNotifyClient(cc *grpc.ClientConn) NotifyClient {
+	return &notifyClient{cc}
+}
+
+func (c *notifyClient) SayWorld(ctx context.Context, in *NotifyReq, opts ...grpc.CallOption) (*NotifyReply, error) {
+	out := new(NotifyReply)
+	err := c.cc.Invoke(ctx, "/helloworld.Notify/SayWorld", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *notifyClient) SayHaha(ctx context.Context, in *NotifyReq, opts ...grpc.CallOption) (*NotifyReply, error) {
+	out := new(NotifyReply)
+	err := c.cc.Invoke(ctx, "/helloworld.Notify/SayHaha", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *notifyClient) SayHi(ctx context.Context, in *NotifyReq, opts ...grpc.CallOption) (*NotifyReply, error) {
+	out := new(NotifyReply)
+	err := c.cc.Invoke(ctx, "/helloworld.Notify/SayHi", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// NotifyServer is the server API for Notify service.
+type NotifyServer interface {
+	// Sends notify
+	SayWorld(context.Context, *NotifyReq) (*NotifyReply, error)
+	SayHaha(context.Context, *NotifyReq) (*NotifyReply, error)
+	SayHi(context.Context, *NotifyReq) (*NotifyReply, error)
+}
+
+func RegisterNotifyServer(s *grpc.Server, srv NotifyServer) {
+	s.RegisterService(&_Notify_serviceDesc, srv)
+}
+
+func _Notify_SayWorld_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(NotifyReq)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(NotifyServer).SayWorld(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/helloworld.Notify/SayWorld",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(NotifyServer).SayWorld(ctx, req.(*NotifyReq))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Notify_SayHaha_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(NotifyReq)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(NotifyServer).SayHaha(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/helloworld.Notify/SayHaha",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(NotifyServer).SayHaha(ctx, req.(*NotifyReq))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Notify_SayHi_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(NotifyReq)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(NotifyServer).SayHi(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/helloworld.Notify/SayHi",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(NotifyServer).SayHi(ctx, req.(*NotifyReq))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Notify_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "helloworld.Notify",
+	HandlerType: (*NotifyServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "SayWorld",
+			Handler:    _Notify_SayWorld_Handler,
+		},
+		{
+			MethodName: "SayHaha",
+			Handler:    _Notify_SayHaha_Handler,
+		},
+		{
+			MethodName: "SayHi",
+			Handler:    _Notify_SayHi_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
